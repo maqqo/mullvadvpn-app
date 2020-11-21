dd9c9226431095a67fe12b991cbbed80795e9fef
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-21 21:17:33 +0000

    Use the new error state cause

--- talpid-core/src/tunnel_state_machine/connecting_state.rs
+++ talpid-core/src/tunnel_state_machine/connecting_state.rs
@@ -422,6 +422,14 @@ impl TunnelState for ConnectingState {
                                             ),
                                         ),
                                     ) => ErrorStateCause::VpnPermissionDenied,
+                                    #[cfg(target_os = "android")]
+                                    tunnel::Error::WireguardTunnelMonitoringError(
+                                        tunnel::wireguard::Error::TunnelError(
+                                            tunnel::wireguard::TunnelError::SetupTunnelDeviceError(
+                                                tun_provider::Error::InvalidDnsServer(address),
+                                            ),
+                                        ),
+                                    ) => ErrorStateCause::InvalidDnsServer(address),
                                     _ => ErrorStateCause::StartTunnelError,
                                 };
                                 ErrorState::enter(shared_values, block_reason)
